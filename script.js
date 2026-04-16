93fc72c54b4d23507c808482935155279ebf17fe
edideias <edyluchese@gmail.com>
2026-04-16 15:33:49 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,27 +2,23 @@ function navigate(page) {
   const app = document.getElementById("app");
 
   if (page === "home") {
-    app.innerHTML = `
-      <div class="card">
-        <h2>Atendimento Rota Fixa 🚗</h2>
-        <p>Você terá sempre o mesmo motorista.</p>
+app.innerHTML = `
+  <div class="card">
+    <h2>Transporte confiável com rotas fixas</h2>
+    <p>Agendamento fácil para trabalho, escola e aeroporto.</p>
 
-        <button onclick="abrirWhatsAppAudio()">
-          🎤 Enviar pedido por áudio
-        </button>
-
-        <p style="margin-top:10px;">
-          Você será direcionado para o WhatsApp para gravar o áudio.
-        </p>
-      </div>
-    `;
+    <button onclick="abrirWhatsAppAudio()">
+      🎤 Enviar por áudio
+    </button>
+  </div>
+`;
   }
 }
 
 navigate("home");
 
 function abrirWhatsAppAudio() {
-  const numero = "5567999999999"; // 🔴 coloque seu número
+  const numero = "5567981434188"; // 🔴 coloque seu número
 
   const mensagem = encodeURIComponent(
 `Olá! Quero fazer um pré-agendamento 🚗
@@ -37,7 +33,25 @@ Vou te enviar um áudio com:
 Você terá sempre o mesmo motorista.`
   );
 
-  const url = `https://wa.me/${numero}?text=${mensagem}`;
+  const url = `https://wa.me/67981434188${numero}?text=${mensagem}`;
 
   window.open(url, "_blank");
+}
+function abrirWhatsAppAudio() {
+  const numero = "5567981434188"; // seu número
+
+  const mensagem = encodeURIComponent(
+`Olá! Quero fazer um pré-agendamento 🚗
+
+Vou te enviar um áudio com:
+- Local de saída
+- Destino
+- Horário
+- Tipo (ida ou ida e volta)
+- Período
+
+Você terá sempre o mesmo motorista.`
+  );
+
+  window.open(`https://wa.me/67981434188${numero}?text=${mensagem}`, "_blank");
 }
